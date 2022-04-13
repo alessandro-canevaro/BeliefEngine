e2b0dabc7aaba78289701a01641cb53c88f31278
Alessandro212977 <s212977@dtu.dk>
2022-04-13 12:02:58 +0200

    something

--- BeliefBase.py
+++ BeliefBase.py
@@ -5,33 +5,34 @@ class BeliefBase:
     def __init__(self):
         self.beliefBase = []
 
-    def add_to_belief_base(self, value):
-        pass
+    def add_to_belief_base(self, newBelief):
+        self.beliefBase.append(to_cnf(newBelief))
 
     def clear_belief_base(self):
-        ''' may be it can be dict'''
-        self.beliefBase = {} 
+        self.beliefBase = [] # may be it can be dict or a set?
     
-    def revision(self, formula):
+    def revision(self, newBelief):
+        formula = to_cnf(newBelief)
+        negFormula = ~formula
+        #do stuff...
+
+    def expansion(self, belief):
         pass
 
-    def expansion(self, belief: Belief):
-        pass
-
-    def contraction(self, belief: Belief):
+    def contraction(self, belief):
         pass
 
     def print_belief(self):
         pass
 
     def show_current_belief_base(self):
-        print ("Current Belief Base: " + self.beliefBase)
+        print ("Current Belief Base: ", self.beliefBase)
     
-        
-
-
+#Not sure we need this class. Maybe is helpful only when we do the part with plausible belief
+"""      
 class Belief:
     ''' it gets the formula to convert it into cnf form '''
     def __init__(self, formula) -> None:
         self.formula = formula
         self.newcnf = to_cnf(formula)
+"""
